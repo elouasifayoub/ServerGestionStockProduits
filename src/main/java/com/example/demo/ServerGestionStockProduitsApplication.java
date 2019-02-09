8901dc7342418c3a9973290b8af2709b3f09857c
ayoubelouasif <ayoubelouasif@gmail.com>
2019-02-09 11:00:18 +0100

    first commit

---
+++ src/main/java/com/example/demo/ServerGestionStockProduitsApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerGestionStockProduitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerGestionStockProduitsApplication.class, args);
+	}
+
+}
+
